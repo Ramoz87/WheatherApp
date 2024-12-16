400729f234057ea54205a46ece697224b4123333
Yury Ramazanov <yury.ramazanov@gmail.com>
2024-12-16 09:08:31 +0100

    Initial commit with project setup and .gitignore file

---
+++ WheatherApp/WheatherApp/WheatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheatherApp.swift
+//  WheatherApp
+//
+//  Created by Yury Ramazanov on 16.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct WheatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
